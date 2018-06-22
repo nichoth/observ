859f1e8440f0a28cb0a3d7f0b48ac6834dba3ad3
Nick Thomas <nichoth@gmail.com>
2018-06-21 17:34:33 -0700

    add sorted collection to index

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var observ = require('observ')
 var struct = require('observ-struct')
 var from = require('./from')
+var SortedCollection = require('./sorted-collection')
 
 module.exports = {
     observ: observ,
     struct: struct,
-    from: from
+    from: from,
+    SortedCollection: SortedCollection
 }
 
