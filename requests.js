16982ce07c293319bf7a25dd5c16ba54618ae9c9
Nick Thomas <nichoth@gmail.com>
2018-07-20 14:17:45 -0700

    add request state

---
+++ requests.js
@@ -0,0 +1,36 @@
+var struct = require('observ-struct')
+var observ = require('observ')
+var xtend = require('xtend')
+
+function Requests () {
+    return struct({
+        isResolving: observ(false),
+        error: observ(null),
+        resolving: struct({})
+    })
+}
+
+Requests.error = function (state, err) {
+    state.error.set(err)
+}
+
+Requests.start = function (state, req) {
+    var newReq = {}
+    newReq[req.id] = req
+    state.set(xtend(state(), {
+        isResolving: true,
+        resolving: xtend(state().resolving, newReq)
+    }))
+}
+
+Requests.resolve = function (state, req) {
+    var reqs = xtend(state().resolving)
+    delete reqs[req.id]
+    state.set(xtend(state(), {
+        isResolving: !!Object.keys(reqs).length,
+        resolving: reqs
+    }))
+}
+
+module.exports = Requests
+
