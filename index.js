8d68e209a773b05b25fa3f10fc7fd490408025f4
Nick Thomas <nichoth@gmail.com>
2018-07-20 14:45:22 -0700

    export requests state

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var observ = require('observ')
 var struct = require('observ-struct')
 var from = require('./from')
 var SortedCollection = require('./sorted-collection')
+var Requests = require('./requests')
 
 module.exports = {
     observ: observ,
     struct: struct,
     from: from,
-    SortedCollection: SortedCollection
+    SortedCollection: SortedCollection,
+    Requests: Requests
 }
 
